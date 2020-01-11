6dd1672f276279b3ad9fee1251bb220b113debdb
fabiannicolas <fabiannicolas38@gmail.com>
2020-01-11 20:53:27 +0100

    musica y otros

---
+++ Assets/Scripts/Music/KeyButtons.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyButtons : MonoBehaviour
+{ 
+    public KeyCode _key;
+    private Button _button;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(_key))
+        {
+            FadeToColor(_button.colors.pressedColor);
+            _button.onClick.Invoke();
+        }
+        else if (Input.GetKeyUp(_key))
+        {
+            FadeToColor(_button.colors.normalColor);
+        }
+    }
+
+    void FadeToColor(Color color)
+    {
+        Graphic graphic = GetComponent<Graphic>();
+        graphic.CrossFadeColor(color, _button.colors.fadeDuration, true, true);
+    }
+}
